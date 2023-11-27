a17f1017a335b399b61b2e5c16f45897c906ba74
zhuyuanji96 <zhuyuanji96@users.noreply.github.com>
2023-11-27 03:26:38 +0000

    deploy: 1c6f9e40feef74917a37035a501771a011080d4b

--- assets/trick-list_index.md.1af04714.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c as s,e as a}from"./app.fbae26df.js";const u=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"trick-list/index.md"}'),n={name:"trick-list/index.md"},o=a("p",null,"技巧",-1),r=[o];function c(i,d,_,l,p,m){return t(),s("div",null,r)}const x=e(n,[["render",c]]);export{u as __pageData,x as default};
